87adeca0512f73305ecb152fcfc3c5982e7b624f
Pablo Kohn <pablokohn@gmail.com>
2022-12-07 10:00:12 -0300

    first commit

---
+++ src/test/java/com/grupo2bbva/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2bbva.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
