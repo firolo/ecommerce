f13f7f8ab3e6d42947af49cfc0a10aa9f9b4e07a
Pablo Kohn <pablokohn@gmail.com>
2022-12-12 11:19:11 -0300

    clases categoria producto y categoria producto, relaciones para base de datos

--- src/main/java/com/grupo2bbva/ecommerce/models/Producto.java
+++ src/main/java/com/grupo2bbva/ecommerce/models/Producto.java
@@ -1,12 +1,23 @@
 package com.grupo2bbva.ecommerce.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Producto {
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY,generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private Long id;
+    private String nombre;
+    private Long stock;
+
+    @OneToMany(mappedBy="producto", fetch= FetchType.EAGER)
+    Set<CategoriaProducto> productoCategorias = new HashSet<>();
+
 
     public Long getId() {
         return id;
@@ -14,5 +25,37 @@ public class Producto {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public Set<CategoriaProducto> getProductoCategorias() {
+        return productoCategorias;
+    }
+
+    public void setProductoCategorias(Set<CategoriaProducto> productoCategorias) {
+        this.productoCategorias = productoCategorias;
+    }
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, Long stock) {
+        this.nombre = nombre;
+        this.stock = stock;
     }
 }
