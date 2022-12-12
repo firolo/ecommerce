dde7986b0f0726ce28d81b658ab4553c183f0c63
Raffi Kocak <raffi.kocak13@gmail.com>
2022-12-12 12:33:10 -0300

    modelado v1 carrito, carritoProducto. creacion DTOs

--- src/main/java/com/grupo2bbva/ecommerce/models/Carrito.java
+++ src/main/java/com/grupo2bbva/ecommerce/models/Carrito.java
@@ -1,10 +1,67 @@
 package com.grupo2bbva.ecommerce.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 public class Carrito {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private Long Id;
+
+    private double montoTotal;
+
+    @OneToMany(mappedBy = "carrito", fetch = FetchType.EAGER)
+    private Set<CarritoProducto> carritoProductos;
+
+    @OneToOne (mappedBy = "carrito", fetch = FetchType.EAGER)
+    @JoinColumn (name = "client_id")
+    private Cliente cliente;
+
+    public Carrito() {
+    }
+
+    public Carrito(Cliente cliente) {
+        this.montoTotal = 0;
+        this.cliente = cliente;
+        this.carritoProductos = new HashSet<>();
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public double getMontoTotal() {
+        return montoTotal;
+    }
+
+    public void setMontoTotal(double montoTotal) {
+        this.montoTotal = montoTotal;
+    }
+
+    public Set<CarritoProducto> getCarritoProductos() {
+        return carritoProductos;
+    }
+
+    public void setCarritoProductos(Set<CarritoProducto> carritoProductos) {
+        this.carritoProductos = carritoProductos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Producto> getProductos() {
+        return carritoProductos.stream().map(carritoProducto -> carritoProducto.getProducto()).collect(Collectors.toList());
+    }
 }
--- src/main/java/com/grupo2bbva/ecommerce/models/CarritoProducto.java
+++ src/main/java/com/grupo2bbva/ecommerce/models/CarritoProducto.java
@@ -1,10 +1,72 @@
 package com.grupo2bbva.ecommerce.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 
 @Entity
 public class CarritoProducto {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "native")
+    @GenericGenerator(name = "native",strategy = "native")
     private Long id;
+
+    private double monto;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "carrito_id")
+    private Carrito carrito;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    private int cantidadProductos;
+
+    public CarritoProducto() {
+    }
+
+    public CarritoProducto(Carrito carrito, Producto producto, int cantidadProductos) {
+        this.cantidadProductos = cantidadProductos;
+        this.monto = producto.getPrecio() * this.cantidadProductos;
+        // hacer carritoProductoUtils?
+        this.carrito = carrito;
+        this.producto = producto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public Carrito getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(Carrito carrito) {
+        this.carrito = carrito;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidadProductos() {
+        return cantidadProductos;
+    }
+
+    public void setCantidadProductos(int cantidad) {
+        this.cantidadProductos = cantidad;
+    }
 }
