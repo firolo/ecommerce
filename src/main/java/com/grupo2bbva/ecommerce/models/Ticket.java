37a756d9210eceba7d4de4438f4bd19faec3fe6b
Ale <alejandrohumaran@yahoo.com.ar>
2022-12-12 09:36:47 -0300

    v2

--- src/main/java/com/grupo2bbva/ecommerce/models/Ticket.java
+++ src/main/java/com/grupo2bbva/ecommerce/models/Ticket.java
@@ -1,4 +1,64 @@
 package com.grupo2bbva.ecommerce.models;
 
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private List <Producto> productos;
+    private double monto;
+    private double subtotal;
+
+
+    public Ticket() { }
+
+    public Ticket(List <Producto> productos, double monto, double subtotal) {
+        this.productos = productos;
+        this.monto = monto;
+        this.subtotal = subtotal;
+    }
+
+    public static void save(Ticket ticket) {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
 }
