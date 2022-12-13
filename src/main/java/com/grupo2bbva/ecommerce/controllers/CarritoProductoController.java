cf33edc88e5fdff8acf9ff8bc786f40751c2a531
Raffi Kocak <raffi.kocak13@gmail.com>
2022-12-13 12:51:14 -0300

    v1 servicios carrito y principio de controller carrito

--- src/main/java/com/grupo2bbva/ecommerce/controllers/CarritoProductoController.java
+++ src/main/java/com/grupo2bbva/ecommerce/controllers/CarritoProductoController.java
@@ -1,4 +1,30 @@
 package com.grupo2bbva.ecommerce.controllers;
 
+import com.grupo2bbva.ecommerce.models.Producto;
+import com.grupo2bbva.ecommerce.services.CarritoProductoService;
+import com.grupo2bbva.ecommerce.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class CarritoProductoController {
+
+    @Autowired
+    private CarritoProductoService carritoProductoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping("/client/carrito/agregar")
+    public Producto agregarProductoACarrito(@RequestParam Long idProducto, @RequestParam int cantidadProducto) {
+        //Averiguar cliente
+        //Producto producto =
+
+        //Cliente.carrito.add
+
+        return null;
+    }
+
+
 }
--- src/main/java/com/grupo2bbva/ecommerce/services/CarritoService.java
+++ src/main/java/com/grupo2bbva/ecommerce/services/CarritoService.java
@@ -1,4 +1,16 @@
 package com.grupo2bbva.ecommerce.services;
 
+import com.grupo2bbva.ecommerce.dtos.CarritoDTO;
+import com.grupo2bbva.ecommerce.models.Carrito;
+
+import java.util.List;
+
 public interface CarritoService {
+    List<Carrito> findAll();
+
+    List<CarritoDTO> findAllDTO();
+
+    Carrito findById(Long id);
+
+    CarritoDTO findByIdDTO(Long id);
 }
